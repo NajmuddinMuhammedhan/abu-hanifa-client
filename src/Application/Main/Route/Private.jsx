61040f95e7fdf913079160de177390b32b99c255
Najmuddin Muhammedhan <najmuddin.muhammedhan@gmail.com>
2020-12-03 00:44:02 +0300

    Changes

--- src/Application/Main/Route/Private.jsx
+++ src/Application/Main/Route/Private.jsx
@@ -1,18 +1,16 @@
-// import { Route, Redirect } from 'react-router-dom'
-import { Route } from 'react-router-dom'
-// import useAccount from '../../Hooks/useAccount.js'
+import { Route, Redirect } from 'react-router-dom'
+import useAccount from '../../Hooks/useAccount.js'
 
 function Private ({ children, ...props }) {
 
-	// const [isLoggedIn] = useAccount()
+	const [isLoggedIn] = useAccount()
 
 	return (
 		<Route
 			{ ...props }
 			render={
 				({ location, }) => {
-					return children
-					// return isLoggedIn !== null ? children : <Redirect to={{ pathname: '/login', state: { from: location, }, }} />
+					return isLoggedIn !== null ? children : <Redirect to={{ pathname: '/login', state: { from: location, }, }} />
 				}
 			}
 		/>
